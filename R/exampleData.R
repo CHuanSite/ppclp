731a19fe0e17a23216a08084e74c105529d1c6ef
Huan Chen <hzchenhuan@gmail.com>
2019-10-18 14:22:53 -0400

    R package for probabilistic principal curve with length penalty

---
+++ R/exampleData.R
@@ -0,0 +1,8 @@
+#' Example number from MNIST dataset
+#'
+#' Number 3 data from MNIST dataset which contains the scaled coordinate of numbre 3
+#' and its start point and end point
+#'
+#' @docType data
+#'
+#' @usage data(threeExample)
